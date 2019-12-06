da7dd568814603ae94954aab386095540b30a9e4
Henrik Nørgaard <menneskefernis@gmail.com>
2019-12-06 20:04:31 +0100

    all methods implemented

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -52,15 +52,55 @@ module Enumerable
     end
 
     def my_count
+        count = 0
+        self.my_each do |item|
+            count += 1 if yield(item)
+        end
+        count
+    end
 
+    def my_map(proc = nil)
+        
+        new_array = []
+
+        self.my_each do |item|
+            if proc || (proc && block_given?)
+                new_array << proc.call(item)
+            else
+                new_array << yield(item)
+            end
+        end
+        
+        new_array        
+        
+    end
+
+    def my_inject(total = 0)
+
+        self.my_each do |item|
+            total = yield(total, item)
+            total = self[0] if total == 0
+        end
+        total
     end
 end
 
 array = [1, 8, 33, 740, 5, 26]
 
-#array.my_each { |number| puts number * 2 }
-#array.my_each_with_index { |number, i| puts "#{number} is at index #{i}" }
-#puts array.my_select { |number| number > 10 }
-#puts array.my_all? { |number| number < 100 }
-#puts array.my_any? { |number| number > 100 }
-puts array.my_none? { |number| number > 100 }
+# array.my_each { |number| puts number }
+# array.my_each_with_index { |number, i| puts "#{number} is at index #{i}" }
+# puts array.my_select { |number| number > 10 }
+# puts array.my_all? { |number| number < 100 }
+# puts array.my_any? { |number| number > 100 }
+# puts array.my_none? { |number| number > 100 }
+# puts array.my_count { |number| number % 2 == 0 }
+# puts array.my_map { |number| number * 2 }
+
+proc = Proc.new { |number| number * 2 }
+puts array.my_map(proc) { |number| number * 3 }
+
+def multiply_els(array)
+    array.my_inject() { |sum, n| sum * n }
+end
+
+# puts multiply_els([2, 4, 5])
